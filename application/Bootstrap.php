a2cd90f043b79dd2ca5054cf3e18eb985576cd9f
dmelo <dmelo@af365ad8-7e5f-4755-afa6-67cbf075a6a7>
2011-07-05 05:36:50 +0000

    initial layout
    git-svn-id: file:///home/dmelo/lightningpacker/trunk@5 af365ad8-7e5f-4755-afa6-67cbf075a6a7

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,15 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    protected function _initView()
+    {
+	$view = new Zend_View($this->getOptions());
+	$view->doctype('HTML5');
+	$view->headMeta()->setCharset('utf-8');
+	//$view->headScript()->appendFile($view->baseUrl() . '/js/jquery.js');
+	$view->headLink()->appendStylesheet($view->baseUrl() . '/css/810.css');
+	$view->headLink()->appendStylesheet($view->baseUrl() . '/css/default.css');
+    }
 
 }
 
