a01f0a90d2b41976e0cbee530e7ae352dada9589
dmelo <dmelo@af365ad8-7e5f-4755-afa6-67cbf075a6a7>
2011-07-27 14:10:08 +0000

    small details
    git-svn-id: file:///home/dmelo/lightningpacker/trunk@61 af365ad8-7e5f-4755-afa6-67cbf075a6a7

--- public/get.php
+++ public/get.php
@@ -1,5 +1,27 @@
 <?php
 
+/**
+ * Interpret hash and type parameters to fetch the right script.
+ *
+ * @author Diogo Oliveira de Melo
+ *
+ * Copyright (C) 2011  Diogo Oliveira de Melo
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *  
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+
 define('CACHE_DIR', 'tmp/');
 define('DEFAULT_EXPIRATION', 30 * 24 * 60 * 60);
 
