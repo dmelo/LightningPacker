71fc1daff916ddf801a8fb0635ec991194e2034b
dmelo <dmelo@af365ad8-7e5f-4755-afa6-67cbf075a6a7>
2011-08-10 23:10:36 +0000

    licecing the code
    git-svn-id: file:///home/dmelo/lightningpacker/trunk@76 af365ad8-7e5f-4755-afa6-67cbf075a6a7

---
+++ scripts/pack_downloads.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Pack all downloadable content
+
+VERSION=`cat VERSION`
+DOWNLOAD_PATH=public/download
+PUBLIC=public/
+ZEND_VIEW=application/views/helpers
+DRUPAL=drupal
+
+rm -rf ${DOWNLOAD_PATH}/*
+
+# Packing PHP.
+PACK_PHP=lightningpacker-php-${VERSION}
+mkdir ${DOWNLOAD_PATH}/${PACK_PHP}
+cp ${PUBLIC}/LightningPacker.php ${DOWNLOAD_PATH}/${PACK_PHP}
+cd ${DOWNLOAD_PATH}
+tar -zcvf ${PACK_PHP}.tar.gz ${PACK_PHP}
+cd -
+
+# Packing Zend Framework
+
+PACK_ZEND=lightningpacker-zend-${VERSION}
+mkdir ${DOWNLOAD_PATH}/${PACK_ZEND}
+cp ${ZEND_VIEW}/LightningPackerScript.php ${ZEND_VIEW}/LightningPackerLink.php ${PUBLIC}/LightningPacker.php ${DOWNLOAD_PATH}/${PACK_ZEND}
+cd ${DOWNLOAD_PATH}
+tar -zcvf ${PACK_ZEND}.tar.gz ${PACK_ZEND}
+cd -
+
+# Packing Drupal Module
+PACK_DRUPAL=lightningpacker-drupal-${VERSION}
+mkdir ${DOWNLOAD_PATH}/${PACK_DRUPAL}
+cp ${DRUPAL}/lightningpacker.module ${DRUPAL}/lightningpacker.info ${PUBLIC}/LightningPacker.php ${DOWNLOAD_PATH}/${PACK_DRUPAL}/
+cd ${DOWNLOAD_PATH}
+tar -zcvf ${PACK_DRUPAL}.tar.gz ${PACK_DRUPAL}
+cd -
